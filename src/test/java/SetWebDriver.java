3194485fd7555a80468e1811cb691a526cd44d63
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-03-23 19:56:32 +0700

    save #8

--- src/test/java/SetWebDriver.java
+++ src/test/java/SetWebDriver.java
@@ -6,10 +6,10 @@ public class SetWebDriver {
     public static WebDriver getDriver(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver","C:\\Program Files\\WebDriver\\bin\\chromedriver-win64\\chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver","C:\\Program Files\\WebDriver\\bin\\Chrome\\chromedriver.exe");
                 return new ChromeDriver();
             case "yandex":
-                System.setProperty("webdriver.chrome.driver","C:\\Program Files\\WebDriver\\bin\\yandexdriver-win64\\yandexdriver.exe");
+                System.setProperty("webdriver.chrome.driver","C:\\Program Files\\WebDriver\\bin\\Yandex\\yandexdriver.exe");
                 return new ChromeDriver();
             default:
                 throw new IllegalArgumentException("Неподдерживаемый браузер:" + browser);
